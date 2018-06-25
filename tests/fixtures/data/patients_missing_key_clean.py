efa904556c138b28297d6adc5584d4a46d570285
Marshall Nichols <marshall.nichols@duke.edu>
2018-06-25 11:41:13 -0400

    Test and Fixtures to reproduce failure
    Added new patient_dirty and clean fixtures where the first record of
    the json input is missing a key. This causes a failure in
    transform_dicts() and will result in a KeyError on test. New test
    added to test_curation.py using these fixtures

---
+++ tests/fixtures/data/patients_missing_key_clean.py
@@ -0,0 +1,16 @@
+data = [
+        {
+            "mrn": "C", 
+            "name": "Josh Winston"
+        },
+        {
+            "mrn": "A", 
+            "name": "Ben Sullivan", 
+            "age": 35
+        },
+        {
+            "mrn": "B", 
+            "name": "Emery Redmond", 
+            "age": 18
+        }
+        ]
